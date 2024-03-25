59ccda67cb0420742e92f6633ed30cadf0fdf8b8
ndt0403 <ndt2704tndv@gmail.com>
2024-03-25 09:53:26 +0700

    Add project files.

---
+++ FPTJobMatch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FPTJobMatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
